7a2cfb215903547ead57ce475e3b5595f3a58aff
douglascoimbra <82838240+douglascoimbra@users.noreply.github.com>
2022-03-23 16:15:26 -0300

    Update product.sql

--- product.sql
+++ product.sql
@@ -1,8 +1,19 @@
 
 SELECT 
-    stg.productname as mdmname,
-    CONCAT("qawork-", stg.productname) as mdmdescription,
-    stg.productprice as mdmunitvalue,
-    CONTAINS_SUBSTR(stg.productname, 'banan') AS mdmDeleted
+    stg.name as mdmname,
+    CONCAT("qawork-", stg.name) as mdmcompanycode,
+    stg.taxid as mdmtaxid,
+    (CONTAINS_SUBSTR(stg.name, 'H') OR CONTAINS_SUBSTR(stg.name, 'I')) AS mdmDeleted,
+  CASE CONTAINS_SUBSTR(stg.name, 'H') OR CONTAINS_SUBSTR(stg.name, 'I')
+      WHEN TRUE THEN 
+        [(STRUCT("REJECTION_RULE" AS mdmStage, CASE CONTAINS_SUBSTR(stg.name, 'H')
+                                                WHEN TRUE THEN "qa work as H"
+                                                          ELSE "qa work as I" 
+                                               END 
+                                               AS mdmErrorMsg, TRUE AS mdmActive))]
+     ELSE []
+  END
+    AS mdmErrors
     --metadata--
-from stg_nlp_product as stg
+from stg_ds167311_emitente_1 as stg
+WHERE stg.name IS NOT NULL
