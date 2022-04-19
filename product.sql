a6acadb9a83e6c2b729ab5c7bee07c5d7d6cf99a
douglascoimbra <82838240+douglascoimbra@users.noreply.github.com>
2022-04-19 11:36:03 -0300

    Update product.sql

--- product.sql
+++ product.sql
@@ -1,7 +1,7 @@
 
 SELECT 
     stg.productname as mdmname,
-    CONCAT("qatest-", stg.productname) as mdmdescription,
+    CONCAT("qawork-", stg.productname) as mdmdescription,
     stg.productprice as mdmunitvalue,
     CONTAINS_SUBSTR(stg.productname, 'banan') AS mdmDeleted
     --metadata--
