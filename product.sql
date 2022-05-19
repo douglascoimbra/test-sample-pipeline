e1c2377eefb41f3b21f83319846e0062dc6f1d5e
douglascoimbra <82838240+douglascoimbra@users.noreply.github.com>
2022-05-19 10:42:35 -0300

    Update product.sql

--- product.sql
+++ product.sql
@@ -1,18 +1,8 @@
-with temp_product as (
-    SELECT
-       stg.productname as mdmname,
-        CONCAT("qadoug-", stg.productname) as mdmdescription,
-        stg.productprice as mdmunitvalue,
-        CONTAINS_SUBSTR(stg.productname, 'banan') AS mdmDeleted,
-        row_number() over (partition by stg.id order by stg.mdmCounterForEntity desc) sequence,
-        --metadata--
-    from stg_nlp_product as stg
-),
-mdmproduct as (
-    select *
-    from temp_product where sequence = 1
-    )
-select *,
-false as mdmDeleted
-from mdmproduct
-
+SELECT 
+    stg.productname as mdmname,
+    CONCAT("qawork-", stg.productname) as mdmdescription,
+    stg.productprice as mdmunitvalue,
+    CONTAINS_SUBSTR(stg.productname, 'banan') AS mdmDeleted
+    --metadata--
+from stg_nlp_product as stg
+--timestamp-- where stg.mdmCounterForEntity > {{start_from}}
