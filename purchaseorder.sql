362d116bbc1ec1ddf89982f63c48f23bd0f65caa
douglascoimbra <82838240+douglascoimbra@users.noreply.github.com>
2022-06-02 08:56:28 -0300

    Update purchaseorder.sql

--- purchaseorder.sql
+++ purchaseorder.sql
@@ -3,7 +3,7 @@ SELECT
     stg.customername as mdmcustomername,
     CONCAT("qawork-", stg.category) as mdmcategoryname,
     stg.country as mdmcountry,
-    CONTAINS_SUBSTR(stg.country, 'Brazil') AS mdmDeleted
+    CONTAINS_SUBSTR(stg.country, 'Brazil') as mdmcountry AS mdmDeleted
     --metadata--
 from stg_nlp_purchaseorder as stg
 --timestamp-- where stg.mdmCounterForEntity > {{start_from}}
