65a6d21e55d645cda940f517742bdd0db3988b93
douglascoimbra <82838240+douglascoimbra@users.noreply.github.com>
2022-07-18 10:38:41 -0300

    Fanout test

---
+++ tableqa.sql
@@ -0,0 +1,9 @@
+SELECT 
+    stg.orderid as mdmorderid,
+    stg.customername as mdmcustomername,
+    stg.country as mdmcountry,
+    CONCAT("qawork-", stg.category) as mdmcategoryname,
+    CONTAINS_SUBSTR(stg.country, 'Brazil') AS mdmDeleted
+    --metadata--
+from stg_nlp_table_qa as stg
+--timestamp-- where stg.mdmCounterForEntity > {{start_from}}
