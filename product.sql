79b00022c79fb14b2d1bc422c8e60f1e3d7a2872
douglascoimbra <82838240+douglascoimbra@users.noreply.github.com>
2023-10-23 13:46:12 -0300

    New Rule Rejn product.sql

--- product.sql
+++ product.sql
@@ -3,7 +3,7 @@ SELECT
     stg.productname as mdmname,
     stg.category as mdmdescription,
     stg.productprice as mdmunitvalue,
-    CONTAINS_SUBSTR(stg.productname, 'banan') AS mdmDeleted
+    CONTAINS_SUBSTR(stg.productname, 'Functionize') AS mdmDeleted
     --metadata{nlp,product}--
 from stg_nlp_product as stg
 where 1=1
