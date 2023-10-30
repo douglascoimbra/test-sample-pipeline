f1e42d0c13797a389d0b3864ac2bbada7fe918c6
douglascoimbra <82838240+douglascoimbra@users.noreply.github.com>
2023-10-30 15:35:21 -0300

    Update farhorizon.sql

--- farhorizon.sql
+++ farhorizon.sql
@@ -3,8 +3,8 @@ SELECT
     stg.productname as mdmname,
     stg.category as mdmdescription,
     stg.productprice as mdmunitvalue,
-    CONTAINS_SUBSTR(stg.productname, 'banan') AS mdmDeleted
-    --metadata{nlp,product}--
+    CONTAINS_SUBSTR(stg.productname, 'functionize') AS mdmDeleted
+    --metadata-v2--
 from stg_nlp_product as stg
 where 1=1
 --timestamp-- and stg.mdmCounterForEntity > {{start_from}}
