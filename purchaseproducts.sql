392fbc31edfdf718823e7fb68ce3400a47023ef6
douglascoimbra <82838240+douglascoimbra@users.noreply.github.com>
2024-02-16 10:12:10 -0300

    Create purchaseproducts.sql

---
+++ purchaseproducts.sql
@@ -0,0 +1,14 @@
+SELECT 
+  stg.orderid as mdmorderid, 
+  stg.customerid as mdmcustomerid, 
+  stg.customername as mdmcustomername, 
+  product.bar_code as mdmbarcode, 
+  stg_p.productname as mdmproductname 
+--metadata-v2--
+FROM 
+  stg_nlp_purchaseorder stg 
+  JOIN UNNEST(stg.products) as product 
+  JOIN stg_nlp_product stg_p on (
+    product.bar_code = stg_p.bar_code
+  ) --timestamp-- and stg.mdmCounterForEntity > {{start_from}}
+  --tenantIds-- and mdmTenantId IN {{tenantIds}}
