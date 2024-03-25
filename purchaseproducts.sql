39a4fbf227ae293bbaef1de5ee99e11601170966
douglascoimbra <82838240+douglascoimbra@users.noreply.github.com>
2024-03-25 19:07:15 -0300

    Update purchaseproducts.sql

--- purchaseproducts.sql
+++ purchaseproducts.sql
@@ -2,13 +2,13 @@ SELECT
   stg.orderid as mdmorderid, 
   stg.customerid as mdmcustomerid, 
   stg.customername as mdmcustomername, 
-  stg.contry as mdmbarcode, 
+  product.bar_code as mdmbarcode, 
   stg_p.productname as mdmproductname 
 --metadata-v2--
 FROM 
   stg_nlp_purchaseorder stg 
   LEFT JOIN UNNEST(stg.products) as product 
   LEFT JOIN stg_nlp_product stg_p on (
-    product.country = stg_p.country
+    product.bar_code = stg_p.bar_code
   ) --timestamp-- and stg.mdmCounterForEntity > {{start_from}}
   --tenantIds-- and mdmTenantId IN {{tenantIds}}
